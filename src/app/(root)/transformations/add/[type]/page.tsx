b32427aaf1a9fc643977ce20a75de5588f890fed
Tharusha Perera <tharushaperera088@gmail.com>
2024-06-13 17:21:50 +0530

    initialize mongodb and cloudinary services

--- src/app/(root)/transformations/add/[type]/page.tsx
+++ src/app/(root)/transformations/add/[type]/page.tsx
@@ -1,5 +1,32 @@
-const AddTransformationTypePage = () => {
-  return <div>AddTransformationTypePage</div>;
+import Header from "@/components/shared/Header";
+import TransformationForm from "@/components/shared/TransformationForm";
+import { transformationTypes } from "@/constants";
+import { getUserById } from "@/lib/actions/user.actions";
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+const AddTransformationTypePage = async ({
+  params: { type },
+}: SearchParamProps) => {
+  const { userId } = auth();
+  const trnsformation = transformationTypes[type];
+
+  if (!userId) redirect("/sign-in");
+  const user = await getUserById(userId);
+
+  return (
+    <>
+      <Header title={trnsformation.title} subtitle={trnsformation.subTitle} />
+      <section className="mt-10">
+        <TransformationForm
+          action="Add"
+          userId={user._id}
+          type={trnsformation.type as TransformationTypeKey}
+          creditBalance={user.creditBalance}
+        />
+      </section>
+    </>
+  );
 };
 
 export default AddTransformationTypePage;
